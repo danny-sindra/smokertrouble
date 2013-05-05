6934703713733b796dc3f05397423d4f276a12c8
Akhiles Leonardus Danny Sindra <achilles_indonesia@yahoo.com>
2013-05-05 21:31:44 +0700

    Add the 'index.php'
    Still testing

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<link rel="stylesheet" href="http://static.stupeflix.com/play/1.2/style-min.css" type="text/css" charset="utf-8"/>
+</head>
+<body>
+	<div class="sxmovie" style="width:640px; height:480px;">
+	<!--
+  <stack duration="24.0">
+      <effect type="none">
+        <video filename = "https://dl.dropboxusercontent.com/u/40340600/project_anti-smoking%20%28part%201%29.mp4"/>
+      </effect>
+	  <overlay left="0.14" width="0.68" top="0.22" height="0.54" margin-start="19.2" duration="5.0">
+        <image filename = "http://www.gamedl.org/wp-content/uploads/2013/03/Resident-Evil-6-Leon-Gameplay.jpg"/>
+      </overlay>
+      <text type="legend">Test Stack</text>
+    </stack>
+	-->
+	</div>
+	
+	
+	<script type="text/javascript">
+	  document.write(unescape('%3Cscript src="http://static.stupeflix.com/play/1.2/play-min.js" type="text/javascript"%3E%3C/script%3E'));
+	</script>
+</body>
+</html>
